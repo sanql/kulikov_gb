670651c38feaeecd2035337387fa2ede322493d3
sanql <88365818+sanql@users.noreply.github.com>
2021-08-04 16:06:00 +0300

    add files lesson_1

---
+++ ex_1.py
@@ -0,0 +1,17 @@
+#Объявление переменных. Присваивание переменным значений.
+lenght = 4
+width = 3
+area = lenght * width
+print('Длина =', lenght)
+print('Ширина =', width)
+print('Площадь =', area)
+
+#Запрос данных о пользователе
+user_name = input('Привет! Введи своё имя - ')
+user_sirname = input('Введи свою фамилию - ')
+user_age = int(input('Введи свой возраст - '))
+user_number = int(input('Введи своё любимое число - '))
+
+print('Тебя зовут ',user_sirname, user_name)
+print('Возраст: ',user_age)
+print('Любимое число: ',user_number)
